5e9876d95e87d361b974a3c89900ea652829cb18
Sridharshini0114 <144587121+Sridharshini0114@users.noreply.github.com>
2023-10-24 19:00:32 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,17 @@
+package whileloop;
+
+public class dowhile {
+
+	public static void main(String[] args) {
+		int i=0;
+		do
+		{
+			System.out.println("I value is:"+i);
+			i=i+1;
+		}while(i<=5);
+			
+	
+
+	}
+
+}
