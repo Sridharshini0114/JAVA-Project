93f636d89fc00d259705f20b6652e17ba3f13f90
Sridharshini0114 <144587121+Sridharshini0114@users.noreply.github.com>
2023-10-25 10:58:50 +0530

    Add files via upload

--- sample.java
+++ sample.java
@@ -1,10 +1,32 @@
-package looping;
+package star;
+import java.util.Scanner;
 
 public class sample {
+	int i,j,row;
+	Scanner sc=new Scanner(System.in);
 
-	public static void main(String[] args) {
-		
+void input()
+{
+	System.out.println("Enter the row value:");
+	row= sc.nextInt();
+}
+void display()
+{
+	for(int i=0;i<row;i++)
+	{
+		for(int j=0;j<=i;j++)
+		{
 
+			System.out.println("*");
+			System.out.println("*");
+		}
 	}
+	
+}
 
+public static void main(String[] args) {
+	sample s1=new sample();
+	s1.input();
+	s1.display();
+}
 }
